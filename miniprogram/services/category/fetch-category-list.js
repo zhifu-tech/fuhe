3ef78c3668f0139939035aa7253b1f9512db3417
Zhifu <zzf.george@gmail.com>
2024-11-03 23:12:21 +0800

    Refactor category list fetching logic to use MobX flow for cancellation support and improved state management

--- miniprogram/services/category/fetch-category-list.js
+++ miniprogram/services/category/fetch-category-list.js
@@ -2,6 +2,7 @@ import log from '@/common/log/log';
 import { saasId } from '@/common/saas/saas';
 import stores from '@/stores/index';
 import models from '@/models/index';
+import { flow } from 'mobx-miniprogram';
 
 let _fetchCategoryListTask = null; // 记录当前正在进行的请求
 
@@ -15,12 +16,12 @@ export default async function ({ tag, trigger }) {
   return _fetchCategoryListTask;
 }
 
-async function _fetchCategoryList({ tag, trigger }) {
+const _fetchCategoryList = flow(function* ({ tag, trigger }) {
   try {
     // 请求中，切换选中状态
     stores.category.setFetchCategoryListStatus({ code: 'loading', trigger });
 
-    const { records: categoryList, total } = await models.category.all({
+    const { records: categoryList, total } = yield models.category.all({
       tag,
       saasId: saasId(),
     });
@@ -32,14 +33,19 @@ async function _fetchCategoryList({ tag, trigger }) {
     stores.category.setFetchCategoryListStatus({ code: 'success', trigger });
     log.info(tag, '_fetchCategoryList result', categoryList);
   } catch (error) {
-    // 请求失败，切换选中状态
-    stores.category.setFetchCategoryListStatus({ code: 'error', trigger });
-    log.error(tag, '_fetchCategoryList error', error);
+    // 判断任务是否被取消
+    if (error.message === 'FLOW_CANCELLED') {
+      log.info(tag, '_fetchCategoryList was cancelled');
+    } else {
+      // 请求失败，切换选中状态
+      stores.category.setFetchCategoryListStatus({ code: 'error', trigger });
+      log.error(tag, '_fetchCategoryList error', error);
+    }
     throw error;
   } finally {
     _fetchCategoryListTask = null;
   }
-}
+});
 
 function _postCategoryList(list) {
   const pinyin = require('js-pinyin');
