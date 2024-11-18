e46acc88ccfd960a400e7ac2d87bc7fe0342e7a1
Zhifu <zzf.george@gmail.com>
2024-11-18 18:37:28 +0800

    Enhanced search functionality in order list to include item details and stock information

--- miniprogram/package-cart/order/list/behaviors/search.js
+++ miniprogram/package-cart/order/list/behaviors/search.js
@@ -1,4 +1,5 @@
 import log from '@/common/log/log';
+import stores from '@/stores/index';
 import orderStore from '../../../stores/order/index';
 
 module.exports = Behavior({
@@ -15,9 +16,30 @@ module.exports = Behavior({
           `${order.createdAtFormatted}` + // 订单创建时间
           `由 ${order.userName} 下单` + // 订单创建人
           `客户 ${order.customerName}` + // 订单客户
-          `商家 ${order.providerName}` + // 订单商家
-          `商品 ${order.spu.title}`; // 订单商品
-        return searchInfo.includes(this.data.searchValue);
+          `商家 ${order.providerName}`; // 订单商家
+        if (searchInfo.includes(this.data.searchValue)) {
+          return true;
+        }
+        return order.itemList.some(({ spuId, skuId, stockId }) => {
+          const spu = stores.goods.getSpu(spuId);
+          const sku = stores.goods.getSku(spuId, skuId);
+          const stock = stores.goods.getStock(spuId, skuId, stockId);
+          if (!spu || !sku || !stock) return false;
+          if (spu.title.includes(this.data.searchValue)) return true;
+          const title = spu.title + sku.optionList.reduce((acc, cur) => acc + ' ' + cur.title, '');
+          if (title.includes(this.data.searchValue)) {
+            return true;
+          }
+          // 供应商匹配
+          const supplierMatch = sku.supplierName?.includes(this.data.searchValue);
+          if (supplierMatch) return true;
+
+          // 库存信息匹配
+          const stockMatch = stock.createdAtFormatted?.includes(this.data.searchValue);
+          if (stockMatch) return true;
+
+          return false;
+        });
       });
     },
     handleSearchChange: function (e) {
